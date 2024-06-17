c999737ee3fc99dbcef97249361ecda17bfbb7b0
Dinusha Madushan <madaduw-se20042@stu.kln.ac.lk>
2024-06-17 08:44:51 +0530

    update my projects part

--- portfolio/src/components/Works/works.js
+++ portfolio/src/components/Works/works.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './works.css';
 import Portfolio1 from '../../assets/portfolio-1.png';
 import Portfolio2 from '../../assets/portfolio-2.png';
-import Portfolio3 from '../../assets/portfolio-3.png';
-import Portfolio4 from '../../assets/portfolio-4.png';
+
 
 
 
@@ -13,10 +12,9 @@ const Works = () => {
         <h2 className='worksTitle'>My Projects</h2>
         <span className='worksDesc'>I take pride in paying attention to the smallest details and making sure that my works is pixel perfect.I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence </span>
         <div className='worksImgs'>
-            <img src={Portfolio1} alt='' className=''/>
-            <img src={Portfolio2} alt='' className=''/>
-            <img src={Portfolio3} alt='' className=''/>
-            <img src={Portfolio4} alt='' className=''/>
+            <img src={Portfolio1} alt='' className='worksImg'/>
+            <img src={Portfolio2} alt='' className='worksImg'/>
+            
         </div>
     </section>
   );
