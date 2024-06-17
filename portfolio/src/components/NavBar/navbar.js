0492f616daf41bd7222f971e93bba4d4e91fc462
Dinusha Madushan <madaduw-se20042@stu.kln.ac.lk>
2024-06-17 20:56:41 +0530

    update mobile responses

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{ useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-scroll';
@@ -19,12 +19,14 @@ const Navbar = () => {
         <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className="detailsMenuListItem">Contact Me</Link>
 
       </div>
-      <button className="desktopMenuBtn" onClick={() =>{
-        document.getElementById('contact').scrollIntoView({behavior:'smooth'});
+      <button className="desktopMenuBtn" onClick={() => {
+        document.getElementById('contactPage').scrollIntoView({behavior:'smooth'});
       }}>
       <img src={contactImg} alt="" className="desktopMenuImg"/>Contact Me</button>
+
+
       <img src={menu} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
-      <div className="navMenu" style={display: showMenu? 'flex' : 'none '} />
+      <div className="navMenu" style={{display: showMenu? 'flex' : 'none '}} >
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
         <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>About Me</Link>
         <Link activeClass='active' to='works' spy={true} smooth={true} offset={-120} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>My Projects</Link>
